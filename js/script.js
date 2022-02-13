b1851aef0daf3f7aa00c99e44f0f3ef2c3d5e476
est5 <ghostrekon5500@ya.ru>
2022-02-13 22:23:25 +0300

    grid to display books

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const addBook = document.querySelector(".submit-book");
 const addBookForm = document.querySelector("#book-form");
 const main = document.querySelector(".main");
 
+tempValues();
+
 showForm.addEventListener("click", () => {
   addBookForm.elements["author"].value = "";
   addBookForm.elements["title"].value = "";
@@ -34,8 +36,33 @@ function addBookToLibrary() {
 
 function createBookCards(book) {
   const card = document.createElement("div");
+  const title = document.createElement("p");
+  const author = document.createElement("p");
+  const button = document.createElement("button");
+
+  title.className = "card-title";
+  author.className = "card-author";
+  button.type = "button";
+  button.classList.add("delete-book");
+  button.innerText = "Delete";
+
+  title.innerText = `Title : ${book["title"]}`;
+  author.innerText = `Auhtor: ${book["author"]}`;
+
+  const buttons = document.createElement;
   card.classList.add("card");
-  card.innerHTML += `Title : ${book["title"]}, Auhtor: ${book["author"]} <br>`;
+  button.id = library.length;
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(button);
 
   main.appendChild(card);
+}
+
+function tempValues() {
+  let a = new Book("some", "value");
+  for (let i = 0; i < 8; i++) {
+    library[i] = a;
+    createBookCards(a);
+  }
 }
