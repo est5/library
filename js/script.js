8f6f45b96b84008ead56b33a331e5bbe7b9e7ecc
est5 <ghostrekon5500@ya.ru>
2022-02-14 15:27:17 +0300

    mvp

--- js/script.js
+++ js/script.js
@@ -3,33 +3,41 @@ const showForm = document.querySelector(".show-form");
 const addBook = document.querySelector(".submit-book");
 const addBookForm = document.querySelector("#book-form");
 const main = document.querySelector(".main");
+let counter = 0;
 
 tempValues();
 
 showForm.addEventListener("click", () => {
+  console.log(library);
+
   addBookForm.elements["author"].value = "";
   addBookForm.elements["title"].value = "";
+  addBookForm.elements["npages"].value = "";
+  addBookForm.elements["read"].checked = false;
   addBookForm.style.visibility == "visible"
     ? (addBookForm.style.visibility = "hidden")
     : (addBookForm.style.visibility = "visible");
 });
 
-function Book(title, author) {
+function Book(title, author, npages, read) {
   this.title = title;
   this.author = author;
+  this.pages = npages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
   let author = addBookForm.elements["author"].value;
   let title = addBookForm.elements["title"].value;
+  let pages = addBookForm.elements["npages"].value;
+  let read = addBookForm.elements["read"].checked;
 
   if (author == "" || title == "") {
     return;
   }
 
-  const book = new Book(title, author);
+  const book = new Book(title, author, pages, read);
   library.push(book);
-  console.log(library);
   addBookForm.style.visibility = "hidden";
   createBookCards(book);
 }
@@ -38,31 +46,69 @@ function createBookCards(book) {
   const card = document.createElement("div");
   const title = document.createElement("p");
   const author = document.createElement("p");
+  const pages = document.createElement("p");
+  const readWrapper = document.createElement("p");
+  const label = document.createElement("label");
+  const read = document.createElement("input");
   const button = document.createElement("button");
 
+  label.setAttribute("for", "isRead" + counter);
+  label.innerText = "Read";
+
+  read.type = "checkbox";
+  read.id = "isRead" + counter;
+  read.name = "isRead";
+  readWrapper.className = "card-read";
+  readWrapper.appendChild(label);
+  readWrapper.appendChild(read);
+  readWrapper.style.display = "flex";
+
+  pages.className = "card-pages";
+
   title.className = "card-title";
+
   author.className = "card-author";
+
   button.type = "button";
   button.classList.add("delete-book");
   button.innerText = "Delete";
 
-  title.innerText = `Title : ${book["title"]}`;
+  title.innerText = `Title: ${book["title"]}`;
   author.innerText = `Auhtor: ${book["author"]}`;
+  pages.innerText = `Number of pages: ${book["pages"]}`;
+  read.checked = book.read;
 
-  const buttons = document.createElement;
   card.classList.add("card");
-  button.id = library.length;
+  button.id = counter;
+  let id = counter;
   card.appendChild(title);
   card.appendChild(author);
+  card.appendChild(pages);
+  card.appendChild(readWrapper);
   card.appendChild(button);
 
   main.appendChild(card);
+
+  button.addEventListener("click", () => {
+    --counter;
+    console.log(counter);
+    library.splice(counter);
+    console.log(library);
+    button.parentNode.remove();
+  });
+
+  read.addEventListener("change", () => {
+    book.read = read.checked;
+  });
+
+  counter++;
 }
 
 function tempValues() {
-  let a = new Book("some", "value");
-  for (let i = 0; i < 8; i++) {
-    library[i] = a;
-    createBookCards(a);
-  }
+  let a = new Book("Lord of the Rings", "J. R. R. Tolkien", 1178, true);
+  let b = new Book("Eloquent JavaScript", "Marijn Haverbeke", 472, false);
+  library.push(a);
+  library.push(b);
+  createBookCards(a);
+  createBookCards(b);
 }
